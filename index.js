880793e95c3e7c22dddafaae2b75695dca1005f9
NetoGuedes10 <gilzethfernandes@gmail.com>
2023-10-11 16:47:40 -0300

    projetoback

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require("express")
-const uuid = require("uuid")
+const express = require("express");
+const uuid = require("uuid");
+import cors from "cors";
 
-const port = 3000
-const app = express()
+const port = 3001
+const app = express();
+app.use(express.json());
+app.use(cors())
 
 
 
 const users = []
 
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ erro: "user not found" })
+    }
+    request.userIndex = index
+    request.userId = id
+    next()
+}
+
 app.get("/users", (request, response) => {
+
     return response.json(users)
 })
 
@@ -24,34 +42,29 @@ app.post("/users", (request, response) => {
 })
 
 
-app.put("/users/:id", (request, response) => {
-    const { id } = request.params
+app.put("/users/:id", checkUserId,(request, response) => {
     const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
 
     const updatedUser = { id, name, age }
 
-    const index = users.findIndex(user => user.id === id)
-
-
-   if(index < 0){
-    return response.status(404).json ({menssage:"user not found"})
-   }
-
-
-
     users[index] = updatedUser
 
-   
     return response.json(updatedUser)
 
 })
 
 
+app.delete("/users/:id",checkUserId, (request, response) => {
+   
+    const index = request.userIndex
 
 
+    users.splice(index, 1)
 
-
-
+    return response.status(204).json()
+})
 
 app.listen(port, () => {
     console.log("lest gol $(port)")
